8228e5f5223c6933eb73c1a52a75ac44178d750d
kai <supon3060@gmail.com>
2023-07-07 18:00:16 +0800

    Update CLI

--- src/lawgpt/cli/cli.py
+++ src/lawgpt/cli/cli.py
@@ -1,10 +1,16 @@
 import typer
 
-from .util import download_and_process_dataset_cli, torchrun
+from .util import download_and_process_dataset_cli, torchrun_lora
+from lawgpt.finetune_lora import train
+
+
+finetune_app = typer.Typer()
+finetune_app.command("torchrun-lora")(torchrun_lora)
+finetune_app.command("lora")(train)
 
 app = typer.Typer()
-app.command("finetune")(torchrun)
 app.command("download-and-process-dataset")(download_and_process_dataset_cli)
+app.add_typer(finetune_app, name="finetune")
 
 if __name__ == "__main__":
     app()
