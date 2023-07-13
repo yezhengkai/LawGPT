dbd6b0c559d9ef1a91d1d31efeb4649ee8b62005
kai <supon3060@gmail.com>
2023-07-13 15:35:54 +0800

    Add webui

--- src/lawgpt/cli/cli.py
+++ src/lawgpt/cli/cli.py
@@ -3,6 +3,7 @@ import typer
 from .util import download_and_process_dataset_cli, torchrun_lora
 from lawgpt.finetune_lora import train
 from lawgpt.infer import main as infer
+from lawgpt.webui import main as webui
 
 
 finetune_app = typer.Typer()
@@ -12,6 +13,7 @@ finetune_app.command("lora")(train)
 app = typer.Typer()
 app.command("download-and-process-dataset")(download_and_process_dataset_cli)
 app.command("infer")(infer)
+app.command("webui")(webui)
 app.add_typer(finetune_app, name="finetune")
 
 if __name__ == "__main__":
